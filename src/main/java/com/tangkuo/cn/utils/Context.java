9a9a03b545dfe963cb20f2529801be992950c393
TANGKUO <616507752@qq.com>
2017-06-21 23:35:17 +0800

    ADD

--- src/main/java/com/tangkuo/cn/utils/Context.java
+++ src/main/java/com/tangkuo/cn/utils/Context.java
@@ -1,6 +1,14 @@
 package com.tangkuo.cn.utils;
 
+import java.util.Map;
+
 import org.apache.commons.collections4.MapUtils;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.NameFilter;
+import com.alibaba.fastjson.serializer.PropertyFilter;
+import com.alibaba.fastjson.serializer.SerializeFilter;
+
 
 /**
  * 
@@ -13,12 +21,28 @@ public class Context {
 	 * 
 	 * @return
 	 */
-	public T context(final ContextHashMap,final T entity,final Map<String,String> filedMap,String[] excludeFileds){
+	public T context(final ContextHashMap contextHashMap,final T entity,final Map<String,String> filedMap,String[] excludeFileds){
+		if(MapUtils.isNotEmpty(contextHashMap) && MapUtils.isNotEmpty(filedMap)){
+			NameFilter nameFilter = (soure,name,value) -> filedMap.get(name);
+			PropertyFilter pf = (soure,name,value) -> !ArrayUtils.contains(excludeFileds.name);
+			String context = JSON.toJSONString(contextHashMap,new SerializeFilter[]{nameFilter,pf});
+			T vo = JSON.parseObject(contextHashMap,(class<T>) entity.getclass());
+			return vo;
+			
+		}
+		return entity;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public T includecontext(final ContextHashMap,final T entity,final Map<String,String> filedMap,String[] excludeFileds){
 		if(MapUtils.isNotEmpty(ContextHashMap) && MapUtils.isNotEmpty(filedMap)){
 			NameFilter nameFilter = (soure,name,value) -> filedMap.get(name);
 			PropertyFilter pf = (soure,name,value) -> !ArrayUtils.contains(excludeFileds.name);
-			String context = JSON.toJSONString(ContextHashMap,new SerialiseFilter[]{nameFilter,pf});
-			T vo = JSON.parsonObject(ContextHashMap,(class<T>) entity.getclass());
+			String context = JSON.toJSONString(ContextHashMap,new SerializeFilter[]{nameFilter,pf});
+			T vo = JSON.parseObject(ContextHashMap,(class<T>) entity.getclass());
 			return vo;
 			
 		}
---
+++ src/main/java/com/tangkuo/cn/utils/ContextHashMap.java
@@ -0,0 +1,12 @@
+package com.tangkuo.cn.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContextHashMap extends HashMap{
+
+	public ContextHashMap() {
+		super();
+	}
+
+}
