52f279b88c918d9057eedeec57abb43155a43a74
TANGKUO <616507752@qq.com>
2017-06-21 23:09:09 +0800

    add

--- src/main/java/com/tangkuo/cn/utils/Context.java
+++ src/main/java/com/tangkuo/cn/utils/Context.java
@@ -1,5 +1,28 @@
 package com.tangkuo.cn.utils;
 
+import org.apache.commons.collections4.MapUtils;
+
+/**
+ * 
+ * @author Administrator
+ *
+ */
 public class Context {
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public T context(final ContextHashMap,final T entity,final Map<String,String> filedMap,String[] excludeFileds){
+		if(MapUtils.isNotEmpty(ContextHashMap) && MapUtils.isNotEmpty(filedMap)){
+			NameFilter nameFilter = (soure,name,value) -> filedMap.get(name);
+			PropertyFilter pf = (soure,name,value) -> !ArrayUtils.contains(excludeFileds.name);
+			String context = JSON.toJSONString(ContextHashMap,new SerialiseFilter[]{nameFilter,pf});
+			T vo = JSON.parsonObject(ContextHashMap,(class<T>) entity.getclass());
+			return vo;
+			
+		}
+		return entity;
+	}
 
 }
