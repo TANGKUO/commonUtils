7b2a2f75fc45ffbb8d8d1ff2ed3381b0a2ce534b
TANGKUO <616507752@qq.com>
2017-06-25 21:45:11 +0800

    add

--- src/main/java/com/tangkuo/cn/utils/Context.java
+++ src/main/java/com/tangkuo/cn/utils/Context.java
@@ -3,48 +3,49 @@ package com.tangkuo.cn.utils;
 import java.util.Map;
 
 import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.ArrayUtils;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.NameFilter;
 import com.alibaba.fastjson.serializer.PropertyFilter;
 import com.alibaba.fastjson.serializer.SerializeFilter;
 
-
 /**
  * 
  * @author Administrator
  *
  */
-public class Context {
-	
+public class Context<T extends Object> {
+
 	/**
 	 * 
 	 * @return
 	 */
-	public T context(final ContextHashMap contextHashMap,final T entity,final Map<String,String> filedMap,String[] excludeFileds){
-		if(MapUtils.isNotEmpty(contextHashMap) && MapUtils.isNotEmpty(filedMap)){
-			NameFilter nameFilter = (soure,name,value) -> filedMap.get(name);
-			PropertyFilter pf = (soure,name,value) -> !ArrayUtils.contains(excludeFileds.name);
-			String context = JSON.toJSONString(contextHashMap,new SerializeFilter[]{nameFilter,pf});
-			T vo = JSON.parseObject(contextHashMap,(class<T>) entity.getclass());
+	public T context(final ContextHashMap contextHashMap, final T entity, final Map<String, String> filedMap,
+			String[] excludeFileds) {
+		if (MapUtils.isNotEmpty(contextHashMap) && MapUtils.isNotEmpty(filedMap)) {
+			NameFilter nameFilter = (soure, name, value) -> filedMap.get(name);
+			PropertyFilter pf = (soure, name, value) -> !ArrayUtils.contains(excludeFileds, name);
+			String context = JSON.toJSONString(contextHashMap, new SerializeFilter[] { nameFilter, pf });
+			T vo = JSON.parseObject(context, (Class<T>) entity.getClass());
 			return vo;
-			
 		}
 		return entity;
 	}
-	
+
 	/**
 	 * 
 	 * @return
 	 */
-	public T includecontext(final ContextHashMap,final T entity,final Map<String,String> filedMap,String[] excludeFileds){
-		if(MapUtils.isNotEmpty(ContextHashMap) && MapUtils.isNotEmpty(filedMap)){
-			NameFilter nameFilter = (soure,name,value) -> filedMap.get(name);
-			PropertyFilter pf = (soure,name,value) -> !ArrayUtils.contains(excludeFileds.name);
-			String context = JSON.toJSONString(ContextHashMap,new SerializeFilter[]{nameFilter,pf});
-			T vo = JSON.parseObject(ContextHashMap,(class<T>) entity.getclass());
+	public T includecontext(final ContextHashMap contextHashMap, final T entity, final Map<String, String> filedMap,
+			String[] excludeFileds) {
+		if (MapUtils.isNotEmpty(contextHashMap) && MapUtils.isNotEmpty(filedMap)) {
+			NameFilter nameFilter = (soure, name, value) -> filedMap.get(name);
+			PropertyFilter pf = (soure, name, value) -> !ArrayUtils.contains(excludeFileds, name);
+			String context = JSON.toJSONString(contextHashMap, new SerializeFilter[] { nameFilter, pf });
+			T vo = JSON.parseObject(context, (Class<T>) entity.getClass());
 			return vo;
-			
+
 		}
 		return entity;
 	}
